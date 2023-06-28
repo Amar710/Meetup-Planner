cb768ab5ca2bc7b1dbf31828c9c7087fcb42e69b
Amardeep <amardeepsangha7@hotmail.com>
2023-06-28 12:59:29 -0700

    first commit

---
+++ src/main/java/com/project/meetupplanner/MeetupplannerApplication.java
@@ -0,0 +1,13 @@
+package com.project.meetupplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/meetupplanner/MeetupplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.meetupplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
