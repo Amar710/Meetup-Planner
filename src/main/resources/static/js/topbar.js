d8e5167131d5332416c25ff3c19e1c3ef2b97f60
Brian <brianlam7250@gmail.com>
2023-07-02 05:13:52 -0700

    additional userProfile features

--- src/main/resources/static/js/topbar.js
+++ src/main/resources/static/js/topbar.js
@@ -1,7 +1,7 @@
 
 //username
 // Get the reference to the <a> element
-var usernameLink = document.getElementById('username-link');
+var usernameLink = document.getElementById('username1');
 
 // get user username from server when they log in
 var username = 'BrianLam'; // Replace with your variable value
@@ -15,15 +15,24 @@ usernameLink.textContent = username;
 
 // JavaScript to toggle the active class on click
 document.addEventListener("DOMContentLoaded", function() {
-var dropdowns = document.getElementsByClassName("dropdown");
-
-for (var i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].addEventListener("click", function() {
-    var content = this.querySelector('.dropdown-content');
-    content.style.display = content.style.display === "block" ? "none" : "block";
+    var dropdowns = document.getElementsByClassName("dropdown");
+  
+    for (var i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].addEventListener("click", function(event) {
+        var content = this.querySelector('.dropdown-content');
+        content.style.display = content.style.display === "block" ? "none" : "block";
+        event.stopPropagation(); // Prevent event propagation to the document
+      });
+    }
+  
+    document.addEventListener("click", function() {
+      // Hide all dropdowns when user clicks anywhere else on the screen
+      for (var i = 0; i < dropdowns.length; i++) {
+        var content = dropdowns[i].querySelector('.dropdown-content');
+        content.style.display = "none";
+      }
     });
-}
-});
+  });
 
 // create function that will set showViewAll
 // get userid and check if admin approval if so set this to true
