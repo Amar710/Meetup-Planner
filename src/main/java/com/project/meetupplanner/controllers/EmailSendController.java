d6b53e92fa18e6eb8f90a1495814e42f913533f7
Luisito Junior Camacho <luisito.camacho@outlook.com>
2023-07-17 12:20:37 -0700

    set up the email being sent to user

---
+++ src/main/java/com/project/meetupplanner/controllers/EmailSendController.java
@@ -0,0 +1,11 @@
+package com.project.meetupplanner.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/mail")
+public class EmailSendController {
+    
+}
