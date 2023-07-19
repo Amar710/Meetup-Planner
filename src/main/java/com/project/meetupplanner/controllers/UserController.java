dffaec2c74030cc32e2a36dec6143f9b26033fb0
Amardeep <amardeepsangha7@hotmail.com>
2023-07-18 18:40:27 -0700

    minor bug fix for controller

--- src/main/java/com/project/meetupplanner/controllers/UserController.java
+++ src/main/java/com/project/meetupplanner/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.project.meetupplanner.controllers;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
