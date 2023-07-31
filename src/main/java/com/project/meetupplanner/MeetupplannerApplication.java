2a5291b7c879d8519b675ec14aaccf846d48d301
Amardeep <amardeepsangha7@hotmail.com>
2023-07-30 19:26:35 -0700

    added security

--- src/main/java/com/project/meetupplanner/MeetupplannerApplication.java
+++ src/main/java/com/project/meetupplanner/MeetupplannerApplication.java
@@ -1,6 +1,7 @@
 package com.project.meetupplanner;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
@@ -12,8 +13,8 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 @SpringBootApplication
 public class MeetupplannerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeetupplannerApplication.class, args);
-	}
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
